b1fac8ce57988a85c67fdc6c1314a38345f97e66
carmen <carmen@269df3cd-7b44-418d-811d-5be6b21a80ad>
2011-07-10 16:32:48 +0000

    thread stuff
    git-svn-id: svn://jraf.org/repos/carmen/FrenchCalendar/trunk@2643 269df3cd-7b44-418d-811d-5be6b21a80ad

--- src/ca/rmen/android/frenchcalendar/FrenchCalendarAppWidget.java
+++ src/ca/rmen/android/frenchcalendar/FrenchCalendarAppWidget.java
@@ -3,20 +3,81 @@ package ca.rmen.android.frenchcalendar;
 import java.io.InputStream;
 import java.util.GregorianCalendar;
 
-import ca.rmen.android.frenchcalendar.common.FrenchCalendarDate;
-import ca.rmen.android.frenchcalendar.common.FrenchCalendarUtil;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Handler;
+import android.content.IntentFilter;
 import android.util.Log;
 import android.widget.RemoteViews;
+import ca.rmen.android.frenchcalendar.common.FrenchCalendarDate;
+import ca.rmen.android.frenchcalendar.common.FrenchCalendarUtil;
 
 public class FrenchCalendarAppWidget extends AppWidgetProvider {
 
+	private static final String BROADCAST_MESSAGE_UPDATE = ".UPDATE_WIDGET";
 	private boolean initialized = false;
 	private FrenchCalendarUtil util = null;
+	private PendingIntent updatePendingIntent = null;
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		debug(context, "received! " + intent.getAction());
+
+		if ((context.getPackageName() + BROADCAST_MESSAGE_UPDATE).equals(intent
+				.getAction())) {
+			final AppWidgetManager appWidgetManager = AppWidgetManager
+					.getInstance(context);
+			final ComponentName provider = new ComponentName(context,
+					FrenchCalendarAppWidget.class);
+			final int[] appWidgetIds = appWidgetManager
+					.getAppWidgetIds(provider);
+			if (appWidgetIds.length == 0)
+				stopWidgetNotifier(context);
+			else
+				updateAll(context, appWidgetManager, appWidgetIds);
+		} else if (Intent.ACTION_SCREEN_ON.equals(intent.getAction())) {
+			startWidgetNotifier(context);
+		} else if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
+			stopWidgetNotifier(context);
+		}
+
+		super.onReceive(context, intent);
+	}
+
+	private void startWidgetNotifier(Context context) {
+		if (updatePendingIntent == null) {
+			updatePendingIntent = createWidgetNotifier(context);
+		}
+		AlarmManager mgr = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		mgr.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), 864,
+				updatePendingIntent);
+		debug(context, "Started updater");
+	}
+
+	private void stopWidgetNotifier(Context context) {
+		debug(context, "Will cancel updater");
+		if (updatePendingIntent == null) {
+			updatePendingIntent = createWidgetNotifier(context);
+		}
+		AlarmManager mgr = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		mgr.cancel(updatePendingIntent);
+		debug(context, "Cancelled updater");
+	}
+
+	private PendingIntent createWidgetNotifier(Context context) {
+		Intent updateIntent = new Intent(context.getPackageName()
+				+ BROADCAST_MESSAGE_UPDATE);
+
+		updatePendingIntent = PendingIntent.getBroadcast(context, 0,
+				updateIntent, 0);
+		return updatePendingIntent;
+	}
 
 	/*
 	 * 
@@ -26,57 +87,52 @@ public class FrenchCalendarAppWidget extends AppWidgetProvider {
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
-		for (int appWidgetId : appWidgetIds)
-			update(context, appWidgetManager, appWidgetId);
+		debug(context, "onUpdate");
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		if (!initialized)
+			init(context, appWidgetManager, appWidgetIds);
 	}
 
 	private void init(final Context context,
-			final AppWidgetManager appWidgetManager, final int appWidgetId) {
-		if (util == null) {
-			Runnable initter = new Runnable() {
-				public void run() {
-					final InputStream equinoxFile = context.getResources()
-							.openRawResource(R.raw.equinoxdates);
-
-					util = new FrenchCalendarUtil(equinoxFile,
-							FrenchCalendarUtil.MODE_ROMME);
-					initialized = true;
-					update(context, appWidgetManager, appWidgetId);
-				}
-			};
-			new Handler().post(initter);
-		}
+			final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
+		startWidgetNotifier(context);
+		IntentFilter filterOn = new IntentFilter(Intent.ACTION_SCREEN_ON);
+		IntentFilter filterOff = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+		context.getApplicationContext().registerReceiver(this, filterOn);
+		context.getApplicationContext().registerReceiver(this, filterOff);
+		initialized = true;
+		debug(context, "initialized");
 	}
 
-	private void update(Context context,
-			final AppWidgetManager appWidgetManager, final int appWidgetId) {
-		debug(context, "update");
-		if (!initialized) {
-			debug(context, "init...");
-			init(context, appWidgetManager, appWidgetId);
-		} else {
-			final RemoteViews views = new RemoteViews(context.getPackageName(),
-					R.layout.appwidget);
-			GregorianCalendar now = new GregorianCalendar();
-			FrenchCalendarDate frenchDate = util.getDate(now);
-			String frenchDateStr = frenchDate.toString();
-			debug(context, frenchDateStr);
-			views.setTextViewText(R.id.text_year, "" + frenchDate.year);
-			views.setTextViewText(R.id.text_dayofmonth, "" + frenchDate.day);
-			debug(context, "weekday " + frenchDate.getDayInWeek());
-			CharSequence weekdayLabel = getLabel(context, R.array.weekdays,
-					frenchDate.getDayInWeek() - 1);
-			CharSequence monthLabel = getLabel(context, R.array.months,
-					frenchDate.month - 1);
-			views.setTextViewText(R.id.text_month, monthLabel);
-			views.setTextViewText(R.id.text_weekday, weekdayLabel);
-			debug(context, now.getTime());
-			String timestamp = String.format("%02d:%02d", frenchDate.hour, frenchDate.minute);
-			views.setTextViewText(R.id.text_time, timestamp);
-			appWidgetManager.updateAppWidget(appWidgetId, views);
-		}
+	public void updateAll(Context context,
+			final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
+		for (int appWidgetId : appWidgetIds)
+			update(context, appWidgetManager, appWidgetId);
+	}
 
+	public void update(Context context,
+			final AppWidgetManager appWidgetManager, final int appWidgetId) {
+
+		final RemoteViews views = new RemoteViews(context.getPackageName(),
+				R.layout.appwidget);
+		GregorianCalendar now = new GregorianCalendar();
+		final InputStream equinoxFile = context.getResources().openRawResource(
+				R.raw.equinoxdates);
+		util = new FrenchCalendarUtil(equinoxFile,
+				FrenchCalendarUtil.MODE_ROMME);
+		FrenchCalendarDate frenchDate = util.getDate(now);
+		views.setTextViewText(R.id.text_year, "" + frenchDate.year);
+		views.setTextViewText(R.id.text_dayofmonth, "" + frenchDate.day);
+		CharSequence weekdayLabel = getLabel(context, R.array.weekdays,
+				frenchDate.getDayInWeek() - 1);
+		CharSequence monthLabel = getLabel(context, R.array.months,
+				frenchDate.month - 1);
+		views.setTextViewText(R.id.text_month, monthLabel);
+		views.setTextViewText(R.id.text_weekday, weekdayLabel);
+		String timestamp = String.format("%02d:%02d:%02d", frenchDate.hour,
+				frenchDate.minute, frenchDate.second);
+		views.setTextViewText(R.id.text_time, timestamp);
+		appWidgetManager.updateAppWidget(appWidgetId, views);
 	}
 
 	private CharSequence getLabel(Context context, int arrayResource, int index) {
@@ -86,8 +142,8 @@ public class FrenchCalendarAppWidget extends AppWidgetProvider {
 			return labels[index];
 		return "";
 	}
-	private void debug(Context context, Object message)
-	{
+
+	private void debug(Context context, Object message) {
 		Log.d(context.getPackageName(), getClass().getName() + ": " + message);
 	}
 }
