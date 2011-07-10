20b8cb428f0b842a4f3bc0544f865ef2bad2a6e4
carmen <carmen@269df3cd-7b44-418d-811d-5be6b21a80ad>
2011-07-10 13:15:28 +0000

    dev
    git-svn-id: svn://jraf.org/repos/carmen/FrenchCalendar/trunk@2640 269df3cd-7b44-418d-811d-5be6b21a80ad

--- src/ca/rmen/android/frenchcalendar/FrenchCalendarAppWidget.java
+++ src/ca/rmen/android/frenchcalendar/FrenchCalendarAppWidget.java
@@ -9,50 +9,86 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 public class FrenchCalendarAppWidget extends AppWidgetProvider {
 
+	private boolean initialized = false;
 	private FrenchCalendarUtil util = null;
+
 	/*
-	
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		update(context);
-		super.onReceive(context, intent);
-	}
-*/
+	 * 
+	 * @Override public void onReceive(Context context, Intent intent) {
+	 * update(context); super.onReceive(context, intent); }
+	 */
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
-		for(int appWidgetId : appWidgetIds)
+		for (int appWidgetId : appWidgetIds)
 			update(context, appWidgetManager, appWidgetId);
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
 	}
-	
-	private void init(Context context)
-	{
-		if(util == null)
-		{
-	        final InputStream equinoxFile = context.getResources().openRawResource(R.raw.equinoxdates);
-			
-			util = new FrenchCalendarUtil(equinoxFile, FrenchCalendarUtil.MODE_ROMME);
+
+	private void init(final Context context,
+			final AppWidgetManager appWidgetManager, final int appWidgetId) {
+		if (util == null) {
+			Runnable initter = new Runnable() {
+				public void run() {
+					final InputStream equinoxFile = context.getResources()
+							.openRawResource(R.raw.equinoxdates);
+
+					util = new FrenchCalendarUtil(equinoxFile,
+							FrenchCalendarUtil.MODE_ROMME);
+					initialized = true;
+					update(context, appWidgetManager, appWidgetId);
+				}
+			};
+			new Handler().post(initter);
 		}
 	}
-	private void update(Context context, final AppWidgetManager appWidgetManager, final int appWidgetId)
-	{
-		Log.d(getClass().getName(),"init...");
-		init(context);
-        Log.d(getClass().getName(), "update");
-        final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget);
-        GregorianCalendar now = new GregorianCalendar();
-        FrenchCalendarDate frenchDate = util.getDate(now);
-        String frenchDateStr = frenchDate.toString();
-        Log.d(getClass().getName(), frenchDateStr);
-        views.setTextViewText(R.id.textView1, frenchDateStr);
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-		
+
+	private void update(Context context,
+			final AppWidgetManager appWidgetManager, final int appWidgetId) {
+		debug(context, "update");
+		if (!initialized) {
+			debug(context, "init...");
+			init(context, appWidgetManager, appWidgetId);
+		} else {
+			final RemoteViews views = new RemoteViews(context.getPackageName(),
+					R.layout.appwidget);
+			GregorianCalendar now = new GregorianCalendar();
+			FrenchCalendarDate frenchDate = util.getDate(now);
+			String frenchDateStr = frenchDate.toString();
+			debug(context, frenchDateStr);
+			views.setTextViewText(R.id.text_year, "" + frenchDate.year);
+			debug(context, "set year");
+			views.setTextViewText(R.id.text_dayofmonth, "" + frenchDate.day);
+			debug(context, "set day");
+			CharSequence weekdayLabel = getLabel(context, R.array.weekdays,
+					frenchDate.getDayInWeek() - 1);
+			CharSequence monthLabel = getLabel(context, R.array.months,
+					frenchDate.month - 1);
+			views.setTextViewText(R.id.text_month, monthLabel);
+			debug(context, "set month");
+			views.setTextViewText(R.id.text_weekday, weekdayLabel);
+			debug(context, "set weekday");
+			appWidgetManager.updateAppWidget(appWidgetId, views);
+			debug(context, "done");
+		}
+
 	}
 
+	private CharSequence getLabel(Context context, int arrayResource, int index) {
+		CharSequence[] labels = context.getResources().getTextArray(
+				arrayResource);
+		if (index >= 0 && index < labels.length)
+			return labels[index];
+		return "";
+	}
+	private void debug(Context context, Object message)
+	{
+		Log.d(context.getPackageName(), getClass().getName() + ": " + message);
+	}
 }
