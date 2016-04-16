d09075e1318bc5393e01fb4c617fe6319e275251
Carmen Alvarez <c@rmen.ca>
2016-04-16 14:46:32 +0200

    Workaround for AlarmManager.setRepeating() completely missing triggers when the device is idle.

--- handheld/src/main/java/ca/rmen/android/frcwidget/FRCWidgetScheduler.java
+++ handheld/src/main/java/ca/rmen/android/frcwidget/FRCWidgetScheduler.java
@@ -27,6 +27,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.util.Log;
 
 import ca.rmen.android.frccommon.Constants;
@@ -49,11 +50,13 @@ public class FRCWidgetScheduler {
     private static FRCWidgetScheduler INSTANCE;
     private final Context context;
     private final PendingIntent updateWidgetPendingIntent;
+    private final PendingIntent updateWidgetTomorrowPendingIntent;
 
     private FRCWidgetScheduler(Context context) {
         this.context = context.getApplicationContext();
         Intent updateWidgetIntent = new Intent(ACTION_WIDGET_UPDATE);
-        updateWidgetPendingIntent = PendingIntent.getBroadcast(context, 0, updateWidgetIntent, 0);
+        updateWidgetPendingIntent = PendingIntent.getBroadcast(context, 0, updateWidgetIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        updateWidgetTomorrowPendingIntent = PendingIntent.getBroadcast(context, 1, updateWidgetIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         IntentFilter filterOn = new IntentFilter(Intent.ACTION_SCREEN_ON);
         IntentFilter filterOff = new IntentFilter(Intent.ACTION_SCREEN_OFF);
         ScreenBroadcastReceiver screenBroadcastReceiver = new ScreenBroadcastReceiver();
@@ -80,21 +83,56 @@ public class FRCWidgetScheduler {
         AlarmManager mgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         // If we only show the date, we will update the widget every day just before midnight
         if (frequency == FRCPreferences.FREQUENCY_DAYS) {
-            Calendar cal = Calendar.getInstance();
-            cal.set(Calendar.HOUR_OF_DAY, 23);
-            cal.set(Calendar.MINUTE, 59);
-            cal.add(Calendar.MINUTE, 1);
-            nextAlarmTime = cal.getTimeInMillis();
+            nextAlarmTime = getTimeTomorrowMidnightMillis();
         }
 
         // Schedule the periodic updates.
         mgr.setRepeating(AlarmManager.RTC, nextAlarmTime, frequency, updateWidgetPendingIntent);
+        scheduleTomorrow();
 
         // Also send a broadcast to force an update now.
         Intent updateIntent = new Intent(ACTION_WIDGET_UPDATE);
         context.sendBroadcast(updateIntent);
 
         Log.v(TAG, "Started updater");
+    }
+
+    /**
+     * A bug (or maybe expected behavior?) has been observed on Marshmallow.
+     * If the widget is supposed to update every night at midnight, the #schedule() method
+     * uses the setRepeating() api.  If the device is idle (doze mode?) at midnight, we would expect the alarm
+     * to be triggered when the device wakes up later.  This isn't the observed case however. It
+     * appears that if the device is idle when a setRepeating alarm is supposed to trigger, that
+     * specific alarm execution is skipped completely, and the alarm won't go off again until the next
+     * scheduled execution (the subsequent day at midnight).  The user impact is that if the user
+     * goes to bed before midnight every day, turning off the device screen before going to bed, the
+     * widget will never (ever!) update.
+     *
+     * We attempt to workaround this by setting an exact alarm for tomorrow at midnight.  With the
+     * setExact api, if the device is idle at midnight, the alarm may not trigger at midnight, but
+     * at least it will trigger when the device wakes up later, which is good enough for us.
+     */
+    public void scheduleTomorrow() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            long nextAlarmTime = getTimeTomorrowMidnightMillis();
+            AlarmManager mgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            mgr.setExact(AlarmManager.RTC, nextAlarmTime, updateWidgetTomorrowPendingIntent);
+        }
+    }
+
+    /**
+     * Perhaps unintuitive but maybe simplest way to get a timestamp for "tomorrow at midnight".
+     * This doesn't return a timestamp of tomorrow at midnight exactly, but tomorrow at midnight plus
+     * X seconds: X is the number of seconds past the current minute.
+     * For example, if right now it's April 16 at 14:42:37.558, this will return the timestamp for
+     * April 17 at 00:00:37.558.
+     */
+    private long getTimeTomorrowMidnightMillis() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.add(Calendar.MINUTE, 1);
+        return cal.getTimeInMillis();
     }
 
     /**
