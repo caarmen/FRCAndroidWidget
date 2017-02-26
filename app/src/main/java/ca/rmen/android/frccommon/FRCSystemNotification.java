1c4356d8e2adf8e686ebd5542555d76fbc975193
Carmen Alvarez <c@rmen.ca>
2017-02-26 17:48:36 +0100

    Update copyright headers.

--- app/src/main/java/ca/rmen/android/frccommon/FRCSystemNotification.java
+++ app/src/main/java/ca/rmen/android/frccommon/FRCSystemNotification.java
@@ -1,6 +1,6 @@
 /*
  * French Revolutionary Calendar Android Widget
- * Copyright (C) 2017 Carmen Alvarez
+ * Copyright (C) 2016-2017 Carmen Alvarez
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
